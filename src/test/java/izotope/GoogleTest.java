1a3db0cec7b92181dfbfc2b2899f522af0c7213e
mamedovartur1 <mamedovartur1@gmail.com>
2020-05-24 20:27:41 +0300

    Google Test

---
+++ src/test/java/izotope/GoogleTest.java
@@ -0,0 +1,40 @@
+package izotope;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class GoogleTest {
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeMethod() {
+        System.setProperty("webdriver.chrome.driver","E:\\Softs\\WebDriver\\chromedriver.exe");
+       driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testMethod() {
+        driver.get("http://google.com");
+        driver.findElement(By.name("q")).sendKeys("toyota");
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        final List<WebElement> elements = driver.findElements(By.cssSelector("div.rc"));
+        for (WebElement el: elements) {
+            Assert.assertTrue(el.getText().toLowerCase().contains("toyota"));
+        }
+    }
+
+    @AfterClass
+    public void afterMethod() {
+        driver.quit();
+    }
+
+}
